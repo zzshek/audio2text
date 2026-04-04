2c0d3de962649f6848cc45038ae2ab05a54ba3a1
root <zzshek@gmail.com>
2026-04-04 14:55:51 +0300

    fix

--- transcriber.py
+++ transcriber.py
@@ -62,8 +62,6 @@ class MLXTranscriber(TranscriberBase):
             path_or_hf_repo=self.model,
             language=self.language,
             word_timestamps=True,
-            beam_size=1,
-            best_of=1,
         )
 
         segments = []
